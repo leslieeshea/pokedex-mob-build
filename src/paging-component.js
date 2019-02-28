27ec91d5bbc53c9856a4ecbe661fd39e1c93c391
Leslie Shea <leslieyshea@gmail.com>
2019-02-28 14:39:02 -0800

    updated paging

--- src/paging-component.js
+++ src/paging-component.js
@@ -8,5 +8,37 @@ const PER_PAGE = 20;
 export default function loadPaging(totalCount) {
     const totalPageCount = Math.ceil(totalCount / PER_PAGE);
     totalPagesSpan.textContent = totalPageCount;
-    
+
+    let currentPageNumber = 1;
+    currentPageSpan.textContent = currentPageNumber;
+
+    previousButton.disabled = currentPageNumber === 1;
+
+    nextButton.addEventListener('click', () => {
+        currentPageNumber++;
+        currentPageSpan.textContent = currentPageNumber;
+
+        const pageOptions = {
+            page: currentPageNumber,
+            perPage: PER_PAGE
+        };
+
+        nextButton.disabled = currentPageNumber === totalPageCount;
+        previousButton.disabled = currentPageNumber === 1;
+
+        console.log(pageOptions);
+    });
+
+    previousButton.addEventListener('click', () => {
+        currentPageNumber--;
+        currentPageSpan.textContent = currentPageNumber;
+
+        const pageOptions = {
+            page: currentPageNumber,
+            perPage: PER_PAGE
+        };
+
+        previousButton.disabled = currentPageNumber === 1;
+        nextButton.disabled = currentPageNumber === totalPageCount;
+    });
 }
